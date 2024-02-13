0b7654b6fe346399c7049e79a01c46d06eea8bd7
GachewaRa <gachewaadrian@gmail.com>
2024-02-13 12:58:41 +0300

    The components have been decoupled, a constant navigation bar has been added, links work when clicked to take you to the clicked page, all conforms to React6; the navbar is not intuitive yet to show the page a user is on

---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+};
+
+function NavBar() {
+    return (
+        <div>
+            <Link to="/" style={linkStyles}>Home</Link>
+            <Link to="/about" style={linkStyles}>About</Link>
+            <Link to="/login" style={linkStyles}>Login</Link>
+        </div>
+    );
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function About() {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-}
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <App/>
+  </BrowserRouter>, document.getElementById("root")
 );
